94e3c77402d54fcb50cfcb6ab81a4327d92f6ac7
Guilherme <guigo613@gmail.com>
2024-02-15 11:11:16 -0300

    implement simple route

---
+++ app/core/route.php
@@ -0,0 +1,28 @@
+<?php
+
+$routes = array(
+    'login' => array('controller' => 'loginController', 'action' => 'index'),
+    'authenticate' => array('controller' => 'loginController', 'action' => 'authenticate'),
+    'addUser' => array('controller' => 'loginController', 'action' => 'add'),
+    'book' => array('controller' => 'bookStoreController', 'action' => 'index'),
+    'addbook' => array('controller' => 'bookStoreController', 'action' => 'add'),
+    'removebook' => array('controller' => 'bookStoreController', 'action' => 'remove'),
+    'logout' => array('controller' => 'loginController', 'action' => 'logout')
+);
+
+function route($routes) {
+    $route = isset($_GET['route']) ? $_GET['route'] : 'login';
+
+    if (array_key_exists($route, $routes)) {
+        $controllerName = $routes[$route]['controller'];
+        $actionName = $routes[$route]['action'];
+
+        require_once 'app/controllers/' . $controllerName . '.php';
+
+        $controller = new $controllerName();
+        $controller->$actionName();
+    } else {
+        header('Location: index.php?route=login');
+        exit();
+    }
+}
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php 
 
-require_once __DIR__."/app/views/login.php";
+session_start();
+
+require_once __DIR__."/app/core/route.php";
+
+route($routes);
